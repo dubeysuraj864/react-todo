8e90f7e73cd929b85a5c187dbad0fd124dd9c702
Suraj Dubey <dubeysuraj864@gmail.com>
2022-08-21 12:46:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+
+  const getLocalItem = () => {
+      let list = localStorage.getItem("list");
+      console.log(list);
+      if(list){
+        return JSON.parse(list)
+      }
+      else{
+        return [];
+      }
+  }
+
+  const [text, setText] = useState("");
+  const [task, setTask] = useState(getLocalItem());
+
+  const changeText = (e) => {
+    setText(e.target.value);
+  };
+  const submitHandler = (e) => {
+    console.log("submitted");
+    e.preventDefault();
+    setTask([...task, text]);
+    setText("")
+    
+  };
+
+  const removeTask = (a) => {
+      const finalData = task.filter((curEle, index) => {
+          return index !== a;
+      })
+
+      setTask(finalData)
+  }
+
+  useEffect(()=>{
+      localStorage.setItem("list", JSON.stringify(task));
+  },[task])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>Todo App </header>
+        <div className="top">
+          <form onSubmit={submitHandler}>
+            <input type="text" value={text} onChange={changeText} />
+            <button type="submit">Add</button>
+       
+          </form>
+       
+
+          <div className="msg"></div>
+        </div>
+        <div className="bottom">
+          {
+          
+            task.map((value,index) => {
+                return(
+                  <>
+               <div className="items">
+               <div className="col-title"><li>{value}</li></div>
+                <div className="col-button"><button onClick={() => removeTask(index) } >Delete</button></div>
+               </div>
+                </>
+                )
+            })
+          }
+        </div>  
+     
+      </div>
     </div>
   );
 }
